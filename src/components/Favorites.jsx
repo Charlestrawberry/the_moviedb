3f65aa6f79de77fe5f81ed0d4c9326d1400c9750
Oduks <charlestrawb_odus@yahoo.com>
2025-04-06 01:44:09 +0100

    changed title in index.html

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Favorites({ favorites, getMovieDetails }) {
   return (
     <div className="mt-8">
-      <h2 className="text-2xl font-semibold mb-4">⭐ Your Favorites</h2>
+      <h2 className="text-2xl font-semibold mb-4">Favorites</h2>
       <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {favorites.map((movie) => (
           <div
